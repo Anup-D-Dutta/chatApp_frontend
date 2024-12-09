7ae8d3e760f56c4e3396017d4db447cd845485be
Anup-D-Dutta <codewithanup@gmail.com>
2024-12-09 19:01:36 +0530

    fixrd frontend

--- src/constants/config.js
+++ src/constants/config.js
@@ -2,6 +2,4 @@
 
 // export const server = import.meta.env.VITE_SERVER;
 
-export const BACKEND_URL = process.env.NODE_ENV === 'PRODUCTION'
-    ? process.env.VITE_SERVER_URL
-    : process.env.VITE_SERVER_PORT;
+export const BACKEND_URL = process.env.VITE_SERVER_URL
