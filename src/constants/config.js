bde6263ed1a21d91f72b53e70e4fdcfde2f50d9b
Anup-D-Dutta <codewithanup@gmail.com>
2024-12-09 18:51:31 +0530

    fixed frontend

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,3 +1,7 @@
 
 
-export const server = import.meta.env.VITE_SERVER;
+// export const server = import.meta.env.VITE_SERVER;
+
+export const BACKEND_URL = process.env.NODE_ENV === 'production'
+    ? process.env.VITE_SERVER_URL
+    : process.env.VITE_SERVER_PORT;
--- src/utils/socket.jsx
+++ src/utils/socket.jsx
@@ -1,6 +1,6 @@
 import {createContext, useContext, useMemo } from "react";
 import io from "socket.io-client"
-import { server } from "../constants/config";
+import { BACKEND_URL } from "../constants/config";
 
 
 const SocketContext = createContext();
@@ -10,7 +10,7 @@ const getSocket= () =>  useContext(SocketContext);
 const SocketProvider = ({children}) => {
 
 const socket = useMemo(() => 
-    io(server, 
+    io(BACKEND_URL, 
         {withCredentials: true}),
         []
     );
