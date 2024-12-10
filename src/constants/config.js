4641acf4674207b9d1743b4b7c31a72d809b6ffd
Anup-D-Dutta <codewithanup@gmail.com>
2024-12-10 20:48:43 +0530

    fixed

--- src/constants/config.js
+++ src/constants/config.js
@@ -2,8 +2,8 @@
 
 // export const server = import.meta.env.VITE_SERVER_PORT;
 
-// export const BACKEND_URL = process.env.VITE_SERVER_URL
+export const API_URL = process.env.VITE_SERVER_URL
 
-export const API_URL = import.meta.env.MODE === "PRODUCTION"
-    ? import.meta.env.VITE_SERVER_URL
-    : import.meta.env.VITE_SERVER_PORT; // For development
+// export const API_URL = import.meta.env.MODE === "PRODUCTION"
+//     ? import.meta.env.VITE_SERVER_URL
+//     : import.meta.env.VITE_SERVER_PORT; // For development
