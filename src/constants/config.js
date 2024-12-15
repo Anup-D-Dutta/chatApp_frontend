714a0baa0c23ced699698a3315bb39198714d4df
Anup-D-Dutta <codewithanup@gmail.com>
2024-12-15 15:26:40 +0530

    fixed

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,7 +1,7 @@
 
 
-export const API_URL = 'https://chatapp-server-v2r1.onrender.com'
+// export const API_URL = 'https://chatapp-server-v2r1.onrender.com'
 
 // export const API_URL = process.env.VITE_SERVER_URL
 
-// export const API_URL = import.meta.env.VITE_SERVER_PORT; 
+export const API_URL = import.meta.env.VITE_SERVER_PORT; 
