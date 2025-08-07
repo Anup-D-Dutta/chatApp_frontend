08ae99bbdfb37cf06b35281f69ebb135f6a7c16f
Anup-D-Dutta <codewithanup@gmail.com>
2025-08-07 13:49:11 +0530

    Fix URL

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,7 +1,5 @@
 
 
-export const API_URL = 'https://chatapp-backend-tviy.onrender.com'
+export const API_URL = "https://chatapp-backend-tviy.onrender.com"
 
-// export const API_URL = process.env.VITE_SERVER_URL
-
-// export const API_URL = import.meta.env.VITE_SERVER_PORT; 
+// export const API_URL = "http://localhost:5000"
